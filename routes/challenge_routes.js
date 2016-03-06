e394a5a060d5e1a00f6906555f7efa3fad4fa28a
Christopher Lee <yensen@gmail.com>
2016-03-05 22:05:30 -0800

    added auth_routes tests and setup challenge routes test file

--- routes/challenge_routes.js
+++ routes/challenge_routes.js
@@ -3,17 +3,17 @@ const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 const jsonParser = require('body-parser').json();
 const Challenge = require(__dirname + '/../models/challenge');
 const handleDBError = require(__dirname + '/../lib/handle_db_error');
-const router = module.exports = exports = express.Router();
+const challengeRouter = module.exports = exports = express.Router();
 
 // future goal: threshold get request
-router.get('/challenges', (req, res) => {
+challengeRouter.get('/challenges', (req, res) => {
   Challenge.find({}, (err, data) => {
     if (err) return handleDBError(err, res);
     res.status(200).json(data);
   });
 });
 
-router.post('/challenges', jwtAuth, jsonParser, (req, res) => {
+challengeRouter.post('/challenges', jwtAuth, jsonParser, (req, res) => {
   var newChallenge = new Challenge(req.body);
   newChallenge.save((err, data) => {
     if (err) return handleDBError(err, res);
@@ -22,18 +22,18 @@ router.post('/challenges', jwtAuth, jsonParser, (req, res) => {
 });
 
 // Add Middleware to check admin privileges
-router.put('/challenges/:id', jwtAuth, jsonParser, (req, res) => {
+challengeRouter.put('/challenges/:id', jwtAuth, jsonParser, (req, res) => {
   var newData = req.body;
   delete newData._id;
   Challenge.update({ _id: req.params.id }, newData, (err) => {
     if (err) return handleDBError(err, res);
-    res.status(200).json({ msg: 'Successfully Update' });
+    res.status(200).json({ msg: 'Successfully Updated Challenge' });
   });
 });
 
-router.delete('/challenges/:id', jwtAuth, jsonParser, (req, res) => {
+challengeRouter.delete('/challenges/:id', jwtAuth, jsonParser, (req, res) => {
   Challenge.remove({ _id: req.params.id }, (err) => {
     if (err) return handleDBError(err, res);
-    res.status(200).json({ msg: 'Successfully Delete' });
+    res.status(200).json({ msg: 'Successfully Deleted Challenge' });
   });
 });
