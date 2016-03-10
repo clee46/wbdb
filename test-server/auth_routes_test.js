1f7d36992a4ac303f0e4413bfd2c3825d2e89daf
Yueyue07 <xww.qyy@gmail.com>
2016-03-10 12:29:43 -0800

    reorganize file

--- test-server/auth_routes_test.js
+++ test-server/auth_routes_test.js
@@ -13,111 +13,115 @@ describe('the authorization route', () => {
   before((done) => {
     this.server = server(4000, done);
   });
-describe('User Signup authentication Test: ', () => {
-  it('should create a new user with a POST request', (done) => {
-    chai.request(baseUri)
-    .post('/api/signup')
-    .send({
-      'username': 'test',
-      'email': 'test@example.com',
-      'password': 'password',
-      'confirmpassword': 'password'
-    })
-    .end((err, res) => {
-      expect(res).to.have.status(200);
-      expect(res.body).to.have.property('token');
-      expect(res.body).to.have.property('msg');
-      done();
-    });
+  after((done) => {
+    this.server.close(done);
   });
-  it('should fail to signup if user did not enter a email', (done) => {
-    var invalidUser = {
-      username: 'test',
-      email: '',
-      password: 'password',
-      confirmpassword: 'password'
-    };
-    chai.request(baseUri)
-    .post('/api/signup')
-    .send(invalidUser)
-    .end((err, res) => {
-      expect(res).to.have.status(400);
-      expect(res.body.msg).to.eql('Please enter an email');
-      done();
-    });
-  });
-
-  it('should fail to signup if user did not enter a valid email', (done) => {
-    var invalidUser = {
-      username: 'test',
-      email: 'test@',
-      password: 'password',
-      confirmpassword: 'password'
-    };
-    chai.request(baseUri)
-      .post('/api/signup')
-      .send(invalidUser)
-      .end((err, res) => {
-        expect(res).to.have.status(400);
-        expect(res.body.msg).to.eql('Please enter a valid email');
-        done();
-      });
-  });
-
-  it('should fail to signup if user did not enter a username', (done) => {
-    var invalidUser = {
-      username: '',
-      email: 'test@example.com',
-      password: 'password',
-      confirmpassword: 'password'
-    };
-    chai.request(baseUri)
-      .post('/api/signup')
-      .send(invalidUser)
-      .end((err, res) => {
-        expect(res).to.have.status(400);
-        expect(res.body.msg).to.eql('Please enter a user name');
-        done();
-      });
+  after((done) => {
+   mongoose.connection.db.dropDatabase(() => {
+     done();
    });
-
-   it('should fail to signup if user fail to enter a password > 7 charachters',
-     (done) => {
-       var invalidUser = {
-         username: 'test',
-         email: 'test@example.com',
-         password: 'passw',
-         confirmpassword: 'passw'
-       };
-       chai.request(baseUri)
-         .post('/api/signup')
-         .send(invalidUser)
-         .end((err, res) => {
-           expect(res).to.have.status(400);
-           expect(res.body.msg).to
-             .eql('Please enter a password longer than 7 characters');
-           done();
-         });
+  });
+  describe('User Signup authentication Test: ', () => {
+    it('should create a new user with a POST request', (done) => {
+      chai.request(baseUri)
+      .post('/api/signup')
+      .send({
+        'username': 'test',
+        'email': 'test@example.com',
+        'password': 'password',
+        'confirmpassword': 'password'
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('token');
+        expect(res.body).to.have.property('msg');
+        done();
+      });
+    });
+    it('should fail to signup if user did not enter a email', (done) => {
+      var invalidUser = {
+        username: 'test',
+        email: '',
+        password: 'password',
+        confirmpassword: 'password'
+      };
+      chai.request(baseUri)
+      .post('/api/signup')
+      .send(invalidUser)
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.msg).to.eql('Please enter an email');
+        done();
+      });
+    });
+    it('should fail to signup if user did not enter a valid email', (done) => {
+      var invalidUser = {
+        username: 'test',
+        email: 'test@',
+        password: 'password',
+        confirmpassword: 'password'
+      };
+      chai.request(baseUri)
+        .post('/api/signup')
+        .send(invalidUser)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.msg).to.eql('Please enter a valid email');
+          done();
+        });
+    });
+    it('should fail to signup if user did not enter a username', (done) => {
+      var invalidUser = {
+        username: '',
+        email: 'test@example.com',
+        password: 'password',
+        confirmpassword: 'password'
+      };
+      chai.request(baseUri)
+        .post('/api/signup')
+        .send(invalidUser)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.msg).to.eql('Please enter a user name');
+          done();
+        });
      });
-
-     it('should be able to check whether the user already exist for signup',
+     it('should fail to signup if user fail to enter a password > 7 charachters',
        (done) => {
-         var sameUser = {
+         var invalidUser = {
            username: 'test',
            email: 'test@example.com',
-           password: 'password',
-           confirmpassword: 'password'
+           password: 'passw',
+           confirmpassword: 'passw'
          };
-          chai.request(baseUri)
-            .post('/api/signup')
-            .send(sameUser)
-            .end((err, res) => {
-              expect(res).to.have.status(400);
-              expect(res.body.msg).to
-                .eql('User already exists! Please use a different username');
-              done();
-            });
-        });
+         chai.request(baseUri)
+           .post('/api/signup')
+           .send(invalidUser)
+           .end((err, res) => {
+             expect(res).to.have.status(400);
+             expect(res.body.msg).to
+               .eql('Please enter a password longer than 7 characters');
+             done();
+           });
+       });
+       it('should be able to check whether the user already exist for signup',
+         (done) => {
+           var sameUser = {
+             username: 'test',
+             email: 'test@example.com',
+             password: 'password',
+             confirmpassword: 'password'
+           };
+            chai.request(baseUri)
+              .post('/api/signup')
+              .send(sameUser)
+              .end((err, res) => {
+                expect(res).to.have.status(400);
+                expect(res.body.msg).to
+                  .eql('User already exists! Please use a different username');
+                done();
+              });
+          });
   });
 
   describe('rest requests that require an existing user in the DB', () => {
@@ -169,20 +173,4 @@ describe('User Signup authentication Test: ', () => {
             });
         });
    });
-
-  after((done) => {
-    this.server.close(done);
-  });
-
-  after((done) => {
-    User.remove({}, (err) => {
-      if (err) console.log(err);
-      done();
-    });
-  });
-  after((done) => {
-   mongoose.connection.db.dropDatabase(() => {
-     done();
-   });
-  });
 });
